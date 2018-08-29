5925f9f589fdf2c70d52f40d67783a9e907e84ff
said <alsaghir.said@gmail.com>
2018-08-29 14:09:40 +0200

    add reducer

--- src/components/Productlist.jsx
+++ src/components/Productlist.jsx
@@ -6,21 +6,35 @@ import {addToCart} from '../modules/actions.js'
 
 // const item = ({products})=
 
-const ProductList = ({items, addToCart}) => (
- <ul>
-   {items.map(product => (
-     <li key={product.id}>
-       <p> {product.title} | {product.price} | {product.inventory}
-       </p>
-       <button className = "addToCart" onClick={()=> addToCart(product)}> Add to Cart </button>
-     </li>
-   ))}
- </ul>
-);
+const ProductList = (props) => {
+
+  const {items, add} = props;
+
+  return (
+     <ul>
+       {items.map(product => (
+         <li key={product.id}>
+           <p> {product.title} | {product.price} | {product.inventory}
+           </p>
+           <button className = "addToCart" onClick={()=> add(product)}> Add to Cart </button>
+         </li>
+       ))}
+     </ul>
+  );
+}
+
+const mapStoreToProps = (store) => ({
+  items: Object.values(store.products)
+})
+
+const mapActionsToProps = {
+  add: addToCart
+}
 
 
 
-export default connect (
- ({products}) => ({
-   items: Object.values(products)
- }), {addToCart})(ProductList);
+
+export default connect(
+  mapStoreToProps,
+  mapActionsToProps
+)(ProductList);
--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -1,12 +1,19 @@
 export function reducer (state = {}, action){
   const {inventory} = state;
+  const {id} = action.item || {}
+
   switch (action.type) {
     case 'ADD_TO_CART':
        return {
          ...state,
-
-       }
-
+          products: {
+            ...state.products,
+            [id]:{
+              ...state.products[id],
+              inventory: products - 1
+            }
+          }
+        }
 
     default:
       return state;
